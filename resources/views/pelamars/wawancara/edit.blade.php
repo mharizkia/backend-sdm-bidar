3124c2492a284b74eefc26ac7f2934ce6e1ff223
mharizkia <mrizkiardiansyah2@gmail.com>
2025-05-19 19:44:33 +0700

    8th

--- resources/views/pelamars/wawancara/edit.blade.php
+++ resources/views/pelamars/wawancara/edit.blade.php
@@ -6,23 +6,24 @@
         <div class="container">
             <h1>Edit Wawancara</h1>
             <div class="mb-3">
-                <label for="pelamar_id" class="form-label">ID Pelamar</label>
-                <input type="text" class="form-control" id="pelamar_id" name="pelamar_id" value="{{ $wawancara->pelamar_id }}" required>
+                <label for="pewawancara_id" class="form-label">Nama PeWawancara</label>
+                <select class="form-select" id="pewawancara_id" name="pewawancara_id" required>
+                    @foreach ($daftarPewawancara as $pewawancara)
+                        <option value="{{ $pewawancara->id }}" {{ $wawancara->pewawancara_id == $pewawancara->id ? 'selected' : '' }}>{{ $pewawancara->jabatan_pewawancara }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="nama_pewawancara" class="form-label">Kesimpulan</label>
+                <input type="text" class="form-control" id="nama_pewawancara" name="nama_pewawancara" value="{{ old('nama_pewawancara', $wawancara->kesimpulan) }}" required>
             </div>
             <div class="mb-3">
                 <label for="tanggal_wawancara" class="form-label">Tanggal Wawancara</label>
-                <input type="date" class="form-control" id="tanggal_wawancara" name="tanggal_wawancara" value="{{ $wawancara->tanggal_wawancara }}" required>
+                <input type="date" class="form-control" id="tanggal_wawancara" name="tanggal_wawancara" value="{{ old('tanggal_wawancara', $wawancara->tanggal_wawancara) }}" required>
             </div>
             <div class="mb-3">
                 <label for="kesimpulan" class="form-label">Kesimpulan</label>
                 <input type="text" class="form-control" id="kesimpulan" name="kesimpulan" value="{{ $wawancara->kesimpulan }}" required>
-            </div>
-            <div class="mb-3">
-                <label for="status" class="form-label">Status</label>
-                <select class="form-select" id="status" name="status" required>
-                    <option value="lulus" {{ $wawancara->status == 'lulus' ? 'selected' : '' }}>Lulus</option>
-                    <option value="tidak_lulus" {{ $wawancara->status == 'tidak_lulus' ? 'selected' : '' }}>Tidak Lulus</option>
-                </select>
             </div>
             <div class="mb-3">
                 <label for="poin_poin_wawancara" class="form-label">File Poin Poin Wawancara (PDF)</label>
@@ -32,12 +33,10 @@
                 @endif
             </div>
             <div class="mb-3">
-                <label for="pewawancara_id" class="form-label
-">Pewawancara</label>
-                <select class="form-select" id="pewawancara_id" name="pewawancara_id" required>
-                    @foreach ($daftarPewawancara as $pewawancara)
-                        <option value="{{ $pewawancara->id }}" {{ $wawancara->pewawancara_id == $pewawancara->id ? 'selected' : '' }}>{{ $pewawancara->jabatan_pewawancara }}</option>
-                    @endforeach
+                <label for="status" class="form-label">Status</label>
+                <select class="form-select" id="status" name="status" required>
+                    <option value="lulus" {{ $wawancara->status == 'lulus' ? 'selected' : '' }}>Lulus</option>
+                    <option value="tidak_lulus" {{ $wawancara->status == 'tidak_lulus' ? 'selected' : '' }}>Tidak Lulus</option>
                 </select>
             </div>
             <button type="submit" class="btn btn-primary">Update</button>
